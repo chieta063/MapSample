1923ad645315c0a9e37b06d65af6c0f2b3d9154c
阿部紘明 <hiroaki@abehiroakinoMacBook-Air.local>
2023-11-14 10:24:38 +0900

    Annotation移動検証

--- MapSample/ContentView.swift
+++ MapSample/ContentView.swift
@@ -5,18 +5,47 @@
 //  Created by 阿部紘明 on 2023/11/08.
 //
 
-import SwiftUI
 import MapKit
+import SwiftUI
 
 extension CLLocationCoordinate2D {
   static let parking = CLLocationCoordinate2D(latitude: 43.09476428626934, longitude: 141.27504567435153)
 }
 
+enum MapMode {
+  case normal
+  case drag
+}
+
 struct ContentView: View {
+  @State var currentMode: MapMode = .normal
+  @State var location: CLLocationCoordinate2D = .parking
+  
   var body: some View {
     MapReader { proxy in
-      Map(interactionModes: .rotate) {
-        Marker("Parking", coordinate: .parking)
+      Map(interactionModes: currentMode == .normal ? .all : .pitch) {
+        Annotation(coordinate: location) {
+          Image(systemName: "mappin")
+            .foregroundStyle(currentMode == .normal ? .red : .green)
+            .onLongPressGesture {
+              print("Long Tap")
+              switch currentMode {
+              case .normal:
+                currentMode = .drag
+              case .drag:
+                currentMode = .normal
+              }
+            }
+            .gesture(DragGesture().onChanged({ value in
+              guard let location = proxy.convert(value.location, from: .global) else {
+                return
+              }
+              print(location)
+              self.location = location
+            }))
+        } label: {
+          
+        }
 //        Annotation("test", coordinate: .parking) {
 //          Text("Parking")
 //        }
@@ -27,9 +56,13 @@ struct ContentView: View {
         }
         print("Tapped at \(location)")
       }
-      .gesture(DragGesture().onChanged({ value in
-        print("Draggd to \(value.location)")
-      }))
+      .gesture(DragGesture().onChanged { value in
+//        print("Draggd to \(value.location)")
+        guard let location = proxy.convert(value.location, from: .local) else {
+          return
+        }
+        self.location = location
+      })
     }
   }
 }
